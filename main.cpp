7843753c8500de67c6d8e7c434d3f8b2b963733c
Lisa Waddell <90416736+techteacher101nj@users.noreply.github.com>
2025-08-30 16:20:11 +0000

    work complete

--- main.cpp
+++ main.cpp
@@ -5,5 +5,13 @@ int main() {
   const double length = 7.2;
   const double width = 1.3;
 
+  double perimeter = 2*(length+width);
+  double area = length*width;
+
+  cout << "Length = " << length << endl;
+  cout << "Width = " << width << endl;
+  cout << "Area = " << area << endl;
+  cout << "Perimeter = " << perimeter << endl;
+
 
 }
